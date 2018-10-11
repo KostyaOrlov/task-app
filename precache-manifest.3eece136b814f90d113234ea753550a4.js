a045dee2d578a945553669268c801ad6547034ce
KostyaOrlov <kostiantyn.orlov@limestonedigital.co>
2018-10-11 22:54:05 +0300

    Updates

---
+++ precache-manifest.3eece136b814f90d113234ea753550a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56213f9ff4d65aed2cfe",
+    "url": "/task-app/static/js/runtime~main.56213f9f.js"
+  },
+  {
+    "revision": "00449d4a0aadc3bea901",
+    "url": "/task-app/static/js/main.00449d4a.chunk.js"
+  },
+  {
+    "revision": "74fcf6ea520e867a527c",
+    "url": "/task-app/static/js/1.74fcf6ea.chunk.js"
+  },
+  {
+    "revision": "00449d4a0aadc3bea901",
+    "url": "/task-app/static/css/main.d8f6dd3a.chunk.css"
+  },
+  {
+    "revision": "74fcf6ea520e867a527c",
+    "url": "/task-app/static/css/1.0cd54d94.chunk.css"
+  },
+  {
+    "revision": "61f083533aed6fdbf9f5a390a056a954",
+    "url": "/task-app/index.html"
+  }
+];
